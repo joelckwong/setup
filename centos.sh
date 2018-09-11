d091dd9a347db4835b2ee8a75a3d1510e0fa39a9
Joel Kwong <joelckwong@yahoo.com>
2018-09-11 12:11:31 -0400

    include credstash, packer and terraform

--- centos.sh
+++ centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y update
-sudo yum -y install wget nc bind-utils
+sudo yum -y install wget nc bind-utils unzip
 curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
 sudo python get-pip.py
 pip install --upgrade pip
@@ -14,3 +14,8 @@ pip install boto
 pip install boto3
 pip install awscli
 pip install -I ansible
+pip install credstash
+curl https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip --output terraform_0.11.8_linux_amd64.zip
+sudo unzip terraform_0.11.8_linux_amd64.zip -d /usr/bin
+curl https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip --output packer_1.2.5_linux_amd64.zip
+sudo unzip packer_1.2.5_linux_amd64.zip -d /usr/bin
