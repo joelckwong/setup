15dbf0a49945f1090eff3771c3ed8b36fbaf4779
joelckwong <31670777+joelckwong@users.noreply.github.com>
2018-10-03 13:31:57 -0400

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -18,4 +18,3 @@ curl https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd6
 sudo unzip terraform_0.11.8_linux_amd64.zip -d /usr/bin
 curl https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip --output packer_1.2.5_linux_amd64.zip
 sudo unzip packer_1.2.5_linux_amd64.zip -d /usr/bin
-exit
