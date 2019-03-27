13295136766c4b5ffbf6c44bff3da1cf29fcc674
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2019-03-27 13:43:44 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -16,8 +16,8 @@ pip install boto3
 pip install awscli
 pip install -I ansible
 pip install credstash
-curl https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip --output terraform_0.11.11_linux_amd64.zip
-sudo unzip terraform_0.11.11_linux_amd64.zip -d /usr/bin
+curl https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.13_linux_amd64.zip --output terraform_0.11.13_linux_amd64.zip
+sudo unzip terraform_0.11.13_linux_amd64.zip -d /usr/bin
 curl https://releases.hashicorp.com/packer/1.3.4/packer_1.3.4_linux_amd64.zip --output packer_1.3.4_linux_amd64.zip
 sudo unzip packer_1.3.4_linux_amd64.zip -d /usr/bin
 sudo cp -p terraform-provider-credstash /usr/bin
