4dd1c09be2b77c09e9333eacbcd1bcf6da70bf23
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2019-05-14 15:18:18 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -18,8 +18,8 @@ pip install -I ansible
 pip install credstash
 curl https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip --output terraform_0.11.13_linux_amd64.zip
 sudo unzip terraform_0.11.13_linux_amd64.zip -d /usr/bin
-curl https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.zip --output packer_1.3.5_linux_amd64.zip
-sudo unzip packer_1.3.5_linux_amd64.zip -d /usr/bin
+curl https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_linux_amd64.zip --output packer_1.4.5_linux_amd64.zip
+sudo unzip packer_1.4.0_linux_amd64.zip -d /usr/bin
 sudo cp -p terraform-provider-credstash /usr/bin
 cat << EOF > ~/.terraformrc
 providers {
