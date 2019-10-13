09d2f2e5207ebc2bed4957e3816f7a36278d57d3
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2019-10-13 18:53:51 -0400

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -15,10 +15,10 @@ pip install boto3
 pip install awscli
 pip install -I ansible
 pip install credstash
-curl https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip --output terraform_0.11.13_linux_amd64.zip
-sudo unzip terraform_0.11.13_linux_amd64.zip -d /usr/bin
-curl https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_linux_amd64.zip --output packer_1.4.0_linux_amd64.zip
-sudo unzip packer_1.4.0_linux_amd64.zip -d /usr/bin
+curl https://releases.hashicorp.com/terraform/0.12.10/terraform_0.12.10_linux_amd64.zip --output terraform_0.12.10_linux_amd64.zip
+sudo unzip terraform_0.12.10_linux_amd64.zip -d /usr/bin
+curl https://releases.hashicorp.com/packer/1.4.4/packer_1.4.4_linux_amd64.zip --output packer_1.4.4_linux_amd64.zip
+sudo unzip packer_1.4.4_linux_amd64.zip -d /usr/bin
 sudo cp -p terraform-provider-credstash /usr/bin
 cat << EOF > ~/.terraformrc
 providers {
