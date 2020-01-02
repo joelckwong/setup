ce9bb999a1fde1afe3cc91bd5441f89d24f10c3c
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2020-01-01 20:25:54 -0500

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -38,3 +38,5 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum -y install docker-ce
 sudo systemctl start docker && sudo systemctl enable docker
 sudo usermod -aG docker $LOGNAME
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
