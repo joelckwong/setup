238e0b9b7b19e6b885a8e3a6eb31f7486be12118
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2020-01-01 20:26:13 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -41,3 +41,5 @@ sudo apt-get -y update
 sudo apt-get -y install docker-ce
 sudo systemctl start docker && sudo systemctl enable docker
 sudo usermod -aG docker $LOGNAME
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
