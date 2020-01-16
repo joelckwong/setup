65336f5e63244006c7772ba8117bc3b3490ce721
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2020-01-15 23:31:17 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,19 +3,18 @@ echo $(hostname -I | cut -d\  -f1) $(hostname) | sudo tee -a /etc/hosts
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install python3 python3-distutils wget netcat-openbsd unzip curl virtualenv make git sshpass
-curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
-sudo python3 get-pip.py
-pip install --upgrade pip
-pip install --user virtualenv
+sudo apt install python3-pip
+pip3 install --upgrade pip
+pip3 install --user virtualenv
 mkdir ~/venv
 virtualenv -p python3 ~/venv
 source ~/venv/bin/activate
 echo "source venv/bin/activate" >> ~/.bash_profile
-pip install boto
-pip install boto3
-pip install awscli
-pip install -I ansible
-pip install credstash
+pip3 install boto
+pip3 install boto3
+pip3 install awscli
+pip3 install -I ansible
+pip3 install credstash
 curl https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip --output terraform_0.12.18_linux_amd64.zip
 sudo unzip terraform_0.12.18_linux_amd64.zip -d /usr/bin
 curl https://releases.hashicorp.com/packer/1.5.1/packer_1.5.1_linux_amd64.zip --output packer_1.5.1_linux_amd64.zip
