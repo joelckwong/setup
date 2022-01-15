a1a0a555407d1859179b4ac69df1ec16789152ee
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2022-01-15 10:42:39 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -23,6 +23,7 @@ sudo mv /tmp/eksctl /usr/bin
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
+terraform -install-autocomplete
 sudo apt-get update && sudo apt-get install packer
 sudo cp -p terraform-provider-credstash /usr/bin
 cat << EOF > ~/.terraformrc
